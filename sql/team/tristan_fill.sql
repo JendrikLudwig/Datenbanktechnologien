17ab90dae7ac72b6c3ab2b6f954a0ef1d5ccf11e
Tristan <42898795+natoriouskpk@users.noreply.github.com>
2022-06-17 10:46:52 +0200

    inserted data into table genre

---
+++ sql/team/tristan_fill.sql
@@ -0,0 +1,37 @@
+// GENRE
+
+insert into genre
+values (1, 'Action');
+
+insert into genre
+values (2, 'Beat em up');
+
+insert into genre
+values (3, 'Shooter');
+
+insert into genre
+values (4, 'Geschicklichkeit');
+
+insert into genre
+values (5, 'Jump n Run');
+
+insert into genre
+values (6, 'Open World');
+
+insert into genre
+values (7, 'Musik');
+
+insert into genre
+values (8, 'Tanz');
+
+insert into genre
+values (9, 'RPG');
+
+insert into genre
+values (10, 'Simulation');
+
+insert into genre
+values (11, 'Sport');
+
+insert into genre
+values (12, 'Strategie');
