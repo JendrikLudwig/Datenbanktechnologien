82a5da6e034378eb1ede84a0cfc021d01de31ad3
Johannes <jojo.jaeger99@gmail.com>
2022-06-24 12:04:14 +0200

    formatted data from table launcher

--- sql/team/johannes_fill.sql
+++ sql/team/johannes_fill.sql
@@ -1,15 +1,13 @@
 // LAUNCHER
 
-insert into launcher
-values (1, 'Airshipper', 'Veloren'),
-(2, 'Battle.Net', 'Blizzard Entertainment'),
-(3, 'GoG', 'CD Projekt'),
-(4, 'Epic Games Store', 'Epic Games'),
-(5, 'Microsoft Store', 'Microsoft'),
-(6, 'Origin', 'Electronic Arts'),
-(7, 'Riot Games', 'Riot Games'),
-(8, 'Steam', 'Valve'),
-(9, 'Uplay', 'Ubisoft');
-
--- optional
--- (69, 'TristanIndustries', 'Grobi');
+INSERT INTO launcher
+VALUES
+	(1, 'Airshipper', 'Veloren'),
+	(2, 'Battle.Net', 'Blizzard Entertainment'),
+	(3, 'GoG', 'CD Projekt'),
+	(4, 'Epic Games Store', 'Epic Games'),
+	(5, 'Microsoft Store', 'Microsoft'),
+	(6, 'Origin', 'Electronic Arts'),
+	(7, 'Riot Games', 'Riot Games'),
+	(8, 'Steam', 'Valve'),
+	(9, 'Uplay', 'Ubisoft');
