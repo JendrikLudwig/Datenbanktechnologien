82e12a5f3dabcb69789dae80d77ac8ebffe1c549
Tristan <42898795+natoriouskpk@users.noreply.github.com>
2022-06-24 13:03:33 +0200

    added data to table game

--- sql/team/tristan_fill.sql
+++ sql/team/tristan_fill.sql
@@ -42,7 +42,7 @@ VALUES
 
 INSERT INTO publisher
 VALUES
-	(1, 'Studio Wildcard'),
+  (1, 'Studio Wildcard'),
   (2, '2K'),
   (3, 'Warner Bros. Games'),
   (4, 'Forest Anderson'),
@@ -53,3 +53,10 @@ VALUES
   (9, 'Porcelain Fortress'),
   (10, 'Larian Studios'),
   (11, 'Electronic Arts');
+
+// GAME
+
+INSERT INTO game
+VALUES
+  (1, 'ARK: Survival Evolved', 15, '16', 1, 1, '2017-08-27', 'Als ein nackte/r Mann/Frau frierend und hungrig auf einer mysteriösen Insel gestrandet, musst du jagen, sammeln, Gegenstände fertigen, Früchte anpflanzen und einen Unterschlupf bauen, um zu überleben. Benutze Geschick und List, um auf ARK lebende Dinosaurier und urzeitliche Lebewesen zu töten, auszubrüten und zu reiten und verbünde dich mit Hunderten von Spielern oder spiele lokal!', '["Einzelspieler", "Mehrspieler", "MMO", "Hardcore Mechanics", "Explore and Discover"]');
+    
