1788d849fa7e98a3ea0a7f42e68e364faa8214a5
Johannes <jojo.jaeger99@gmail.com>
2022-06-24 14:12:21 +0200

    added data in table launchergame

--- sql/team/johannes_fill.sql
+++ sql/team/johannes_fill.sql
@@ -12,3 +12,32 @@ VALUES
 	(8, 'Steam', 'Valve'),
 	(9, 'Uplay', 'Ubisoft'),
     (10, 'Webbrowser', '');
+
+
+    //LAUNCHERGAME
+
+    INSERT INTO launchergame
+VALUES
+	(1, 1, 3, 'https://store.epicgames.com/de/p/ark'),
+    (2, 1, 8, 'https://store.steampowered.com/app/346110/ARK_Survival_Evolved/'),
+    (3, 2, 3, 'https://store.epicgames.com/de/p/sid-meiers-civilization-vi'),
+    (4, 2, 8, 'https://store.steampowered.com/app/289070/Sid_Meiers_Civilization_VI/'),
+    (5, 3, 8, 'https://store.steampowered.com/app/976310/Mortal_Kombat11/'),
+    (6, 4, 1, 'https://veloren.net/'),
+    (7, 5, 8, 'https://store.steampowered.com/app/202970/Call_of_Duty_Black_Ops_II/'),
+    (8, 6, 10, 'https://justdancenow.com/'),
+    (9, 7, 3, 'https://store.epicgames.com/de/p/cyberpunk-2077'),
+    (10, 7, 4, 'https://www.gog.com/de/game/cyberpunk_2077'),
+    (11, 7, 8, 'https://store.steampowered.com/app/1091500/Cyberpunk_2077/'),
+    (12, 8, 7, 'https://www.leagueoflegends.com/de-de/'),
+    (13, 9, 3, 'https://store.epicgames.com/de/p/rocket-league'),
+    (14, 10, 7, 'https://playruneterra.com/en-us/'),
+    (15, 11, 8, 'https://store.steampowered.com/app/359550/Tom_Clancys_Rainbow_Six_Siege/'),
+    (16, 11, 9, 'https://store.ubi.com/de/tom-clancy-s-rainbow-six-siege/56c494ad88a7e300458b4d5a.html'),
+    (17, 12, 8, 'https://store.steampowered.com/app/829660/No_Time_to_Relax/'),
+    (18, 13, 4, 'https://www.gog.com/de/game/divinity_original_sin_2'),
+    (19, 13, 8, 'https://store.steampowered.com/app/435150/Divinity_Original_Sin_2__Definitive_Edition/'),
+    (20, 14, 3, 'https://store.epicgames.com/de/p/star-wars-battlefront-2'),
+    (21, 14, 6, 'https://www.origin.com/deu/de-de/store/star-wars/star-wars-battlefront-2'),
+    (22, 15, 6, 'https://www.origin.com/deu/de-de/store/need-for-speed/need-for-speed-heat'),
+    (23, 15, 8, 'https://store.steampowered.com/app/1222680/Need_for_Speed_Heat/');
