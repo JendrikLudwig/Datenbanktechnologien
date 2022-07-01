fea9eb63b49312962cc15bd7d469e9576321bb8a
JendrikLaptop <jendrik.ludwig@outlook.de>
2022-07-01 06:39:06 +0200

    Added short svelte example

--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -1,61 +1,19 @@
-<script context="module">
-	export const prerender = true;
-</script>
-
 <script>
-	import Counter from '$lib/Counter.svelte';
+
+    let data;
+
+    async function load() {
+        data = await fetch("https://api.chucknorris.io/jokes/random").then(x => {return x.json()})//.then(x => {return JSON.stringify(x)}) 
+        console.log(data); 
+    }
+
 </script>
+<style></style>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</span>
 
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<h1>Wilkommen in unserem Projek</h1>
+<button on:click={load}>Daten Laden</button>
+<div>
+    {data?.value}
+</div>
