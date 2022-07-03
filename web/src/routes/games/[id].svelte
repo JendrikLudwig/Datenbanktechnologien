238e84a0c402853a77d2afda933f6604782be657
Jendrik Ludwig <jendrik.ludwig@outlook.de>
2022-07-03 17:32:38 +0200

    Added single game site

---
+++ web/src/routes/games/[id].svelte
@@ -0,0 +1,1 @@
+<h1>Leere Seite</h1>
--- web/src/routes/games/index.svelte
+++ web/src/routes/games/index.svelte
@@ -1,5 +1,5 @@
 <script>
-  import LoginProvider from "../comp/providers/login_provider.svelte";
+  import LoginProvider from "../../comp/providers/login_provider.svelte"
   import GameCard from "../comp/game/gamecard.svelte";
 
   let data = [];
