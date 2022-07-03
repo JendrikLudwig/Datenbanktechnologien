ee2788e23f73469f9ba35e64f220609b3af741ff
Jendrik Ludwig <jendrik.ludwig@outlook.de>
2022-07-03 15:37:12 +0200

    Example setup

---
+++ web/src/routes/games.svelte
@@ -0,0 +1,34 @@
+<script>
+    import LoginProvider from "../comp/providers/login_provider.svelte"
+    import GameCard from "../comp/game/gamecard.svelte"
+
+
+    let data = [];
+
+
+    async function loadgames() {
+        const response = await fetch("/api/game.json")
+        .then(res => {return res.json()})
+        
+        data = response
+    }
+
+</script>
+<style>
+
+
+</style>
+
+
+<LoginProvider>
+    <button on:click={loadgames}>Senden</button>
+
+    {#each data as game}
+        <GameCard>{game.Title}</GameCard>
+
+    {/each}
+
+</LoginProvider>
+
+
+
---
+++ web/src/routes/singleGame.svelte
@@ -0,0 +1,23 @@
+<script>
+const data = 
+{
+    GameID: 15,
+    Title: 'Need For Speed: Heat',
+    Genre: 13,
+    FSK: '12',
+    Developer: 15,
+    Publisher: 11,
+    ReleaseDate: "2019-11-07T23:00:00.000Z",
+    Description: 'Rase am Tag und riskiere alles in der Nacht – in der Need for Speed Heat Deluxe Edition, einem atemberaubenden Streetracing-Erlebnis, in dem die Grenzen des Gesetzes verblassen, sobald die Sonne untergeht.',
+    Features: '["Einzelspieler", "Online-PvP", "Online-PvE", "Peripherie"]'
+  }
+
+  //Launcher (vlt sogar als Link zu den passenden Seiten)
+
+
+
+
+
+</script>
+
+
